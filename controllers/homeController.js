43b47b63917b159b3ae984dd91733af5e77db705
AndrewAyman7 <104999702+AndrewAyman7@users.noreply.github.com>
2022-05-29 00:04:14 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,18 +1,18 @@
-const modelProducts = require("../models/products")             // Import (DB Page) , to use its functions
+const modelProducts = require("../models/products")             
 
 
-exports.homeget = (req,res,next)=>{         // Export 
+exports.homeget = (req,res,next)=>{        
 
 
     let category = req.query.category
 
     if (category && category !== "all" ) {
-        modelProducts.getFilteredProducts(category).then(products=>{          // use the fun  
-            res.render("index" , { products : products , isAdmin : req.session.isAdmin } )                    // Pass the data to index.ejs 
+        modelProducts.getFilteredProducts(category).then(products=>{          
+            res.render("index" , { products : products , isAdmin : req.session.isAdmin } )                   
         })
     } else {
-        modelProducts.getProducts().then(products=>{                          // use the fun  
-            res.render("index" , { products : products,  isUser : req.session.userId,  invalid : req.flash( "validErrors")[0], isAdmin : req.session.isAdmin } )  // Pass the data to index.ejs 
+        modelProducts.getProducts().then(products=>{                         
+            res.render("index" , { products : products,  isUser : req.session.userId,  invalid : req.flash( "validErrors")[0], isAdmin : req.session.isAdmin } )  
             
         })
     }
