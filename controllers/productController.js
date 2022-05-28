bf80478758de6c594adf42eede8f92e947f25b26
AndrewAyman7 <104999702+AndrewAyman7@users.noreply.github.com>
2022-05-29 00:04:28 +0200

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,10 @@
-const modelProducts = require("../models/products")    // Import (DB Page) , to use its function
+const modelProducts = require("../models/products")    
 
 exports.productget = (req,res,next)=>{
 
     let id = req.params.id
 
     modelProducts.getProductById(id).then( (product)=>{
-        res.render("productDetails" ,  {product:product , isUser : req.session.userId , invalid : req.flash( "validErrors")[0], isAdmin : req.session.isAdmin })        // new page 
+        res.render("productDetails" ,  {product:product , isUser : req.session.userId , invalid : req.flash( "validErrors")[0], isAdmin : req.session.isAdmin })    
     } )
 }
