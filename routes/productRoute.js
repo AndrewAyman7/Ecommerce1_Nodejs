e8fcc2446484485ada70b6b65fa2b2305d40e229
AndrewAyman7 <104999702+AndrewAyman7@users.noreply.github.com>
2022-05-29 00:06:16 +0200

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const productController = require("../controllers/productController") // import
+const productController = require("../controllers/productController")
 
 router.get("/:id" , productController.productget) 
 
